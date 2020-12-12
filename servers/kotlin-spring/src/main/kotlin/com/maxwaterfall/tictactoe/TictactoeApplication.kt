af56c554f8e0c973c3b37a0d7c2213f705420f0c
Max Waterfall <maximuswaterfall@gmail.com>
2020-12-12 19:30:03 +0000

    Initial setup for kotlin-spring server.

---
+++ servers/kotlin-spring/src/main/kotlin/com/maxwaterfall/tictactoe/TictactoeApplication.kt
@@ -0,0 +1,11 @@
+package com.maxwaterfall.tictactoe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TictactoeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TictactoeApplication>(*args)
+}
